9ad8ce9dfa3e8dd69e001323500cab4436676baa
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:50:36 +0100

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for models/city.py.
-Unittest classes:
-    TestCity_instantiation
-    TestCity_save
-    TestCity_to_dict
+"""
+Defines unittests for models/city.py.
 """
 import os
 import models
@@ -14,7 +11,9 @@ from models.city import City
 
 
 class TestCity_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the City class."""
+    """
+    Unittests for testing instantiation of the City class.
+    """
 
     def test_no_args_instantiates(self):
         self.assertEqual(City, type(City()))
@@ -90,7 +89,9 @@ class TestCity_instantiation(unittest.TestCase):
 
 
 class TestCity_save(unittest.TestCase):
-    """Unittests for testing save method of the City class."""
+    """
+    Unittests for testing save method of the City class.
+    """
 
     @classmethod
     def setUp(self):
@@ -141,7 +142,9 @@ class TestCity_save(unittest.TestCase):
 
 
 class TestCity_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the City class."""
+    """
+    Unittests for testing to_dict method of the City class.
+    """
 
     def test_to_dict_type(self):
         self.assertTrue(dict, type(City().to_dict()))
