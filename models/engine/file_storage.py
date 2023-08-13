269ee73342a72b072f3e5767c53214efe1abd738
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:09:24 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""The FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -11,11 +11,7 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
+    """Represent an abstracted storage engine."""
     __file_path = "file.json"
     __objects = {}
 
