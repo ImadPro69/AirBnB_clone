ab51f458f5e1fa585327623207eecf9c76055d58
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:14:49 +0100

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/state.py.
-Unittest classes:
-    TestState_instantiation
-    TestState_save
-    TestState_to_dict
-"""
+"""Unittests for models/state.py."""
 import os
 import models
 import unittest
