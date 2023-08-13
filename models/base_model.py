edc87976c0bf07c7267cee417d8d48a4eef59dce
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:45:59 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,6 @@ Parent class to all classes in the AirBnB clone project
 
 class BaseModel():
     """Parent class for AirBnB clone project
-    Methods:
-        __init__(self, *args, **kwargs)
-        __str__(self)
-        __save(self)
-        __repr__(self)
-        to_dict(self)
     """
 
     def __init__(self, *args, **kwargs):
@@ -56,10 +50,7 @@ class BaseModel():
 
     def save(self):
         """
-        Instance method to:
-        - update current datetime
-        - invoke save() function &
-        - save to serialized file
+        Instance method
         """
         self.updated_at = datetime.now()
         models.storage.save()
