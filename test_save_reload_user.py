689425a9978b8a06bf33540de42bc29d9815507b
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:43:53 +0100

    Update test_save_reload_user.py

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -3,6 +3,8 @@ from models import storage
 from models.base_model import BaseModel
 from models.user import User
 
+#test_save_reload_user.py
+
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
