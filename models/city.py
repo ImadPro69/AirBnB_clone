ace7b106009b6d773485803ef3e469ca4a94b287
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:46:12 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,9 +8,6 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """
     A subclass of BaseModel class
-    Public class attributes:
-        state_id: (str) will be State.id
-        name:     (str)
     """
     state_id = ""
     name = ""
