32cf5d3c5813e33f4e4ea69004fdf81043f43ab9
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:55:00 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py.
-
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
+"""
+Defines unittests for console.py.
 """
 import os
 import pep8
@@ -25,23 +16,31 @@ from unittest.mock import patch
 
 
 class TestHBNBCommand_prompting(unittest.TestCase):
-    """Unittests for testing prompting of the HBNB command interpreter."""
+    """
+    Unittests for testing prompting of the HBNB command interpreter.
+    """
 
     @classmethod
     def setUpClass(self):
-        """Set up test"""
+        """
+        Set up test
+        """
         self.typing = console.HBNBCommand()
 
     @classmethod
     def tearDownClass(self):
-        """Remove temporary file (file.json) created as a result"""
+        """
+        Remove temporary file (file.json) created as a result
+        """
         try:
             os.remove("file.json")
         except FileNotFoundError:
             pass
 
     def test_pep8_console(self):
-        """Pep8 console.py"""
+        """
+        Pep8 console.py
+        """
         style = pep8.StyleGuide(quiet=False)
         errors = 0
         file = (["console.py"])
@@ -49,7 +48,9 @@ class TestHBNBCommand_prompting(unittest.TestCase):
         self.assertEqual(errors, 0, 'Need to fix Pep8')
 
     def test_pep8_test_console(self):
-        """Pep8 test_console.py"""
+        """
+        Pep8 test_console.py
+        """
         style = pep8.StyleGuide(quiet=False)
         errors = 0
         file = (["tests/test_console.py"])
@@ -57,11 +58,15 @@ class TestHBNBCommand_prompting(unittest.TestCase):
         self.assertEqual(errors, 0, 'Need to fix Pep8')
 
     def test_docstrings_in_console(self):
-        """Test docstrings exist in console.py"""
+        """
+        Test docstrings exist in console.py
+        """
         self.assertTrue(len(console.__doc__) >= 1)
 
     def test_docstrings_in_test_console(self):
-        """Test docstrings exist in test_console.py"""
+        """
+        Test docstrings exist in test_console.py
+        """
         self.assertTrue(len(self.__doc__) >= 1)
 
     def test_prompt_string(self):
@@ -74,7 +79,9 @@ class TestHBNBCommand_prompting(unittest.TestCase):
 
 
 class TestHBNBCommand_help(unittest.TestCase):
-    """Unittests for testing help messages of the HBNB command interpreter."""
+    """
+    Unittests for testing help messages of the HBNB command interpreter.
+    """
 
     def test_help_quit(self):
         h = "Quit command to exit the program"
@@ -137,7 +144,9 @@ class TestHBNBCommand_help(unittest.TestCase):
 
 
 class TestHBNBCommand_exit(unittest.TestCase):
-    """Unittests for testing exiting from the HBNB command interpreter."""
+    """
+    Unittests for testing exiting from the HBNB command interpreter.
+    """
 
     def test_quit_exits(self):
         with patch("sys.stdout", new=StringIO()) as output:
@@ -149,7 +158,9 @@ class TestHBNBCommand_exit(unittest.TestCase):
 
 
 class TestHBNBCommand_create(unittest.TestCase):
-    """Unittests for testing create from the HBNB command interpreter."""
+    """
+    Unittests for testing create from the HBNB command interpreter.
+    """
 
     @classmethod
     def setUp(self):
@@ -231,7 +242,9 @@ class TestHBNBCommand_create(unittest.TestCase):
 
 
 class TestHBNBCommand_show(unittest.TestCase):
-    """Unittests for testing show from the HBNB command interpreter"""
+    """
+    Unittests for testing show from the HBNB command interpreter
+    """
 
     @classmethod
     def setUp(self):
@@ -484,7 +497,9 @@ class TestHBNBCommand_show(unittest.TestCase):
 
 
 class TestHBNBCommand_destroy(unittest.TestCase):
-    """Unittests for testing destroy from the HBNB command interpreter."""
+    """
+    Unittests for testing destroy from the HBNB command interpreter.
+    """
 
     @classmethod
     def setUp(self):
@@ -738,7 +753,9 @@ class TestHBNBCommand_destroy(unittest.TestCase):
 
 
 class TestHBNBCommand_all(unittest.TestCase):
-    """Unittests for testing all of the HBNB command interpreter."""
+    """
+    Unittests for testing all of the HBNB command interpreter.
+    """
 
     @classmethod
     def setUp(self):
@@ -866,7 +883,9 @@ class TestHBNBCommand_all(unittest.TestCase):
 
 
 class TestHBNBCommand_update(unittest.TestCase):
-    """Unittests for testing update from the HBNB command interpreter."""
+    """
+    Unittests for testing update from the HBNB command interpreter.
+    """
 
     @classmethod
     def setUp(self):
@@ -1118,7 +1137,9 @@ class TestHBNBCommand_update(unittest.TestCase):
 
 
 class TestHBNBCommand_count(unittest.TestCase):
-    """Unittests for testing count method of HBNB comand interpreter."""
+    """
+    Unittests for testing count method of HBNB comand interpreter.
+    """
 
     @classmethod
     def setUp(self):
