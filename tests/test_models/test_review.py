e0f2c7377565132c7bb5b2ad8c04f18f6b797cae
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-08-13 21:14:29 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/review.py.
-Unittest classes:
-    TestReview_instantiation
-    TestReview_save
-    TestReview_to_dict
-"""
+"""Unittests for models/review.py."""
 import os
 import models
 import unittest
